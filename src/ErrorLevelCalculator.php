8b2e7d0f14ecf9b415c1c8c89949dba3674159a0
David G. Paul <jedi58@users.noreply.github.com>
2016-01-15 23:05:17 +0000

    Update ErrorLevelCalculator.php

--- src/ErrorLevelCalculator.php
+++ src/ErrorLevelCalculator.php
@@ -1,26 +1,41 @@
 <?php
 
 class ErrorLevelCalculator {
-	protected static $levels = array(
-		'E_ALL', 'E_ERROR', 'E_WARNING', 'E_PARSE', 'E_NOTICE', 'E_CORE_ERROR', 
-		'E_CORE_WARNING', 'E_COMPILE_ERROR', 'E_COMPILE_WARNING', 'E_STRICT',
-		'E_USER_ERROR', 'E_USER_WARNING', 'E_USER_NOTICE'
-	);
-	public function toString($value)
-	{
-    	$output = array();
-    	$seperator = ' | ';
-    	if ($value > 2048) {
-    		$output[] = 'E_ALL';
-    		$seperator = ' & ~';
-    		$value = E_ALL - $value;
-    	}
-    	foreach (self::$levels as $code => $name) {
-    		if (($value & (int) constant($name)) == (int) constant($name)) {
-				$output[] = $name;
-				$value &= ~(int) constant($name);
-    		}
-    	}
-    	return implode($seperator, $output);
-	}
+    protected static $levels = array(
+        'E_ALL', 'E_ERROR', 'E_WARNING', 'E_PARSE', 'E_NOTICE', 'E_CORE_ERROR', 
+        'E_CORE_WARNING', 'E_COMPILE_ERROR', 'E_COMPILE_WARNING', 'E_STRICT',
+        'E_USER_ERROR', 'E_USER_WARNING', 'E_USER_NOTICE'
+    );
+    public function toString($value)
+    {
+        $output = array();
+        $seperator = ' | ';
+        if ($value > 2048) {
+            $output[] = 'E_ALL';
+            $seperator = ' & ~';
+            $value = E_ALL - $value;
+        }
+        foreach (self::$levels as $code => $name) {
+            if (($value & (int) constant($name)) == (int) constant($name)) {
+                $output[] = $name;
+                $value &= ~(int) constant($name);
+            }
+        }
+        return implode($seperator, $output);
+    }
+
+    public function toCode($value)
+    {
+        $levels = preg_split('(\||\&)', str_replace('~', '', $value));
+        $output = 0;
+        foreach ($levels as $level) {
+            $level = trim($level);
+            if ((!empty($level) && $level == 'E_ALL') || $output < 2048) {
+                $output += (int) constant($level);
+            } else {
+                $output &= ~(int) constant($level);
+            }
+        }
+        return $output;
+    }
 }
