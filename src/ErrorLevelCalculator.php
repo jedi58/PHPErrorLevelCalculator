ae1010d6ad3e340e61e5d9574bf74854b152faac
David G. Paul <jedi58@users.noreply.github.com>
2016-01-15 16:17:37 +0000

    Create ErrorLevelCalculator.php
    Initial version of Error Level Calculator which will convert integers into string representations of the PHP error level

---
+++ src/ErrorLevelCalculator.php
@@ -0,0 +1,24 @@
+class ErrorLevelCalculator {
+	protected static $levels = array(
+		'E_ALL', 'E_ERROR', 'E_WARNING', 'E_PARSE', 'E_NOTICE', 'E_CORE_ERROR', 
+		'E_CORE_WARNING', 'E_COMPILE_ERROR', 'E_COMPILE_WARNING', 'E_STRICT',
+		'E_USER_ERROR', 'E_USER_WARNING', 'E_USER_NOTICE'
+	);
+	public function toString($value)
+	{
+    	$output = array();
+    	$seperator = ' | ';
+    	if ($value > 2048) {
+    		$output[] = 'E_ALL';
+    		$seperator = ' & ~';
+    		$value = E_ALL - $value;
+    	}
+    	foreach (self::$levels as $code => $name) {
+    		if (($value & (int) constant($name)) == (int) constant($name)) {
+				$output[] = $name;
+				$value &= ~(int) constant($name);
+    		}
+    	}
+    	return implode($seperator, $output);
+	}
+}
