eb6da4f6190ff3ae24d9ecb82fbb11757e3a64fa
David G. Paul <jedi58@users.noreply.github.com>
2016-01-15 23:16:04 +0000

    Create PHPErrorLevelCalculatorTest.php
    Add unit tests

---
+++ tests/phpunit/PHPErrorLevelCalculatorTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @group unit
+ */
+class PHPErrorLevelCalculatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testSingleErrorCodeToString()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toCode('E_ALL'), 32767);
+    }
+    
+    public function testSingleErrorStringToCode()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toCode(32767), 'E_ALL');
+    }
+    
+    public function testStringAllNoWarnings()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toCode('E_ALL & ~E_WARNING'), 32765);
+    }
+    
+    public function testStringErrorsAndWarnings()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toCode('E_ERROR | E_WARNING'), 3);
+    }
+    
+    public function testCodeAllNoNoticesNoStrict()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toString(6135), 'E_ALL & ~E_NOTICE & ~E_STRICT');
+    }
+    
+    public function testCodeAllNoWarnings()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toString(32765), 'E_ALL & ~E_WARNING');
+    }
+    
+    public function testCodeErrorsAndWarnings()
+    {
+        $this->assertEquals(ErrorLevelCalculator::toString(3), 'E_ERROR | E_WARNING');
+    }
+}
